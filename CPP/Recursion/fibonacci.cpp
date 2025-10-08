81ba2266579055fc51da09b7cbca10d54cc412e2
itskundan-01 <2300030371@kluniversity.in>
2025-10-08 23:30:09 +0530

    pushing latest changes

---
+++ CPP/Recursion/fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+ 
+int main(){
+    int n;
+    cout<<"Enter the value of N: ";
+    cin>>n;
+    int res = fib(n);
+    cout<<res<<endl;
+}
---
+++ CPP/Strivers/Pattern Printing/pattern22.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter the value of N: ";
+    int n;
+    cin>>n;
+    for(int i=1;i<2*n;i++){
+        for(int j=1;j<2*n;j++){
+            //Needed to write the algo. currently Incomplete
+        }
+        cout<<endl;
+    }
+    return 0;
+}
