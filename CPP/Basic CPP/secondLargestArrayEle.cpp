d4fce38a36b5dc8cd037084a7b7ca10e7b1e6e74
itskundan-01 <2300030371@kluniversity.in>
2025-10-17 23:44:05 +0530

    Added secondLargestArray file

---
+++ CPP/Basic CPP/secondLargestArrayEle.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of Array: ";
+    cin>>n;
+    int Arr[n];
+    int lar,sLar;
+    cout<<"Enter the Array elements: ";
+    for(int i=0;i<n;i++){
+        cin>>Arr[i];
+        if(i==0){
+             lar = Arr[0];
+            sLar = lar;
+        }
+        if(lar<Arr[i]) lar = Arr[i];
+        if(sLar>Arr[i]) sLar = Arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        if(Arr[i]<lar && Arr[i]>sLar){
+            sLar = Arr[i];
+        }
+    }
+
+    cout<<"The Second Largest element of the Array is: "<<sLar<<endl;
+
+}
+
