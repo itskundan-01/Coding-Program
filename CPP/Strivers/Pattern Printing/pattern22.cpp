939ef0849fedc0a9ce260e88a9b5ae06fde7d131
itskundan-01 <2300030371@kluniversity.in>
2025-10-26 23:34:43 +0530

    Updated the pattern22 file

--- CPP/Strivers/Pattern Printing/pattern22.cpp
+++ CPP/Strivers/Pattern Printing/pattern22.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i=1;i<2*n;i++){
         for(int j=1;j<2*n;j++){
             //Needed to write the algo. currently Incomplete
-            //Will do it 
+            //Will do it as this is gonna be done by calculating the 
         }
         cout<<endl;
     }
