b20a692d25a20a33b197f955cdde64078b7434a5
itskundan-01 <2300030371@kluniversity.in>
2025-12-23 22:53:43 +0530

    Add reverse number implementation in Basic_Maths

---
+++ CPP/Strivers/Basic_Maths/reverseNumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int rev=0;
+    while(n>0){
+        rev = (rev*10)+n%10;
+        n /=10;
+    }
+    cout<<"The reversed number is: "<<rev<<endl;
+}
