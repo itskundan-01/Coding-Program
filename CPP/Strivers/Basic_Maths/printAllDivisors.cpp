b2d5445c515476facca6b8e273550ad23fbfa64f
itskundan-01 <2300030371@kluniversity.in>
2025-12-24 22:34:27 +0530

    Add implementation to print all divisors of a number in Basic_Maths

---
+++ CPP/Strivers/Basic_Maths/printAllDivisors.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void divisor(int n){
+    int count = 0;
+    for(int i = 1; i <= sqrt(n); i++){
+        if(n % i == 0){
+            count++;          // count i as divisor
+            if(i != n/i)      // avoid counting sqrt(n) twice
+                count++;      // count n/i as divisor
+        }
+    }
+    cout << "Total number of divisors: " << count;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    divisor(n);
+}
