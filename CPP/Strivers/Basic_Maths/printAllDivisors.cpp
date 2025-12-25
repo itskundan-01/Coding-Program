5a1f2cb8d9d5faba94fd9bd0abecca49645107ab
itskundan-01 <2300030371@kluniversity.in>
2025-12-25 16:10:52 +0530

    Refactor divisor function to improve output formatting and readability

--- CPP/Strivers/Basic_Maths/printAllDivisors.cpp
+++ CPP/Strivers/Basic_Maths/printAllDivisors.cpp
@@ -8,9 +8,12 @@ void divisor(int n){
     int count = 0;
     for(int i = 1; i <= sqrt(n); i++){
         if(n % i == 0){
+            cout<<i<<'\t';
             count++;          // count i as divisor
-            if(i != n/i)      // avoid counting sqrt(n) twice
+            if(i != n/i){     // avoid counting sqrt(n) twice
                 count++;      // count n/i as divisor
+                cout<<n/i<<'\t';
+            }
         }
     }
     cout << "Total number of divisors: " << count;
