d26bcd60ec58f3f597d6dbd3cbcdbde785d4c1b1
itskundan-01 <2300030371@kluniversity.in>
2025-12-25 22:42:54 +0530

    Add sumOfNNumbers.cpp to implement functionality for calculating the sum of the first N natural numbers using recursion

---
+++ CPP/Strivers/Recursion/sumOfNNumbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+    if(n==0) return 0;
+    return n+(sum(n-1));
+    
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int s = sum(n);
+    cout<<"The sum is: "<<s<<endl;
+}
