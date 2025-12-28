609b818d48b8daf6d86d6f0c123fbe42498d7e27
itskundan-01 <2300030371@kluniversity.in>
2025-12-28 23:05:04 +0530

    Add bubbleSort.cpp to implement bubble sort algorithm

---
+++ CPP/Strivers/Sorting/bubbleSort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void bubble_sort(int* arr,int n){
+    for(int a=0;a<n;a++) cin>>arr[a];
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+        }
+    }
+    for(int i=0;i<n;i++)cout<<arr[i]<<"\t";
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    bubble_sort(arr,n);
+    
+}
