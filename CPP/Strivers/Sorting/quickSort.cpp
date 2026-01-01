c5d94c012d2858a969a3dde6fde219739027c344
itskundan-01 <2300030371@kluniversity.in>
2026-01-01 15:54:00 +0530

    Refactor quick sort implementation for improved clarity and efficiency

--- CPP/Strivers/Sorting/quickSort.cpp
+++ CPP/Strivers/Sorting/quickSort.cpp
@@ -1,28 +1,31 @@
 #include<iostream>
 using namespace std;
 
-int qs(int* arr, int low,int high){
-    int i=low, j =high, pivot =low;
-    if(low<high){
-        
-        while(i<j){
-            while(arr[i]<=arr[pivot] && i<=high) i++;
-            while(arr[j]>arr[pivot] && j>=low) j--;
-            if(i<j) swap(arr[i],arr[j]);  
-        }
-        if(j<i) swap(arr[j],arr[pivot]);
+int partition(int* arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+
+    while (true) {
+        while (i <= high && arr[i] <= pivot) i++;
+        while (arr[j] > pivot) j--;
+
+        if (i >= j) break;
+        swap(arr[i], arr[j]);
     }
+
+    swap(arr[low], arr[j]);
     return j;
 }
 
-void quickSort(int* arr,int low, int high){
-    if(low<high){
-        int pivot = low;
-        int pIndex = qs(arr,low,high);
-        quickSort(arr,low,pIndex-1);
-        quickSort(arr,pIndex+1,high);
+void quickSort(int* arr, int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
     }
 }
+
 
 int main(){
     int n;
