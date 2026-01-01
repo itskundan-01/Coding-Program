25023b44a5a890f85afd2bc356b4fadea0ef3dfb
itskundan-01 <2300030371@kluniversity.in>
2026-01-01 17:23:31 +0530

    Add largestElement.cpp to find the largest element in an array

---
+++ CPP/Strivers/Array Problems/Easy/largestElement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    int max;
+    for(int i=0;i<n;i++){
+        cin>>v.at(i);
+        if(i==0) max=v.at(0);
+        if(v.at(i)>max) max = v.at(i);
+    }
+    cout<<"The largest element is: "<<max;
+}
