49369c6c5b2a6bd21df816234ecbc146f1791d23
itskundan-01 <2300030371@kluniversity.in>
2026-01-02 00:25:42 +0530

    Add isSorted.cpp to check if an array is rotated sorted

--- CPP/Strivers/Array Problems/Easy/isSorted.cpp
+++ CPP/Strivers/Array Problems/Easy/isSorted.cpp
@@ -1,10 +1,26 @@
 #include<iostream>
 using namespace std;
+
+bool isSortedRotated(int arr[], int n) {
+    int count = 0;
+    for(int i=0; i<n; i++) {
+        if(arr[i] > arr[(i+1)%n]) count++;
+    }
+    return (count <= 1);
+}
+
+
 int main(){
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
+
+    bool isSorted = isSortedRotated(arr,n);
+    if(isSorted==true)cout<<endl<<"The array is Rotated Sorted"<<endl;
+    else cout<<"\nThe array is not rotated Sorted"<<endl;
+
+    //Non rotated sorted check
     for(int i=1;i<n;i++){
         if(arr[i]<arr[i-1]){
             cout<<"False";
