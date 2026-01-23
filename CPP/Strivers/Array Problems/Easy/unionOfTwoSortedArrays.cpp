75aa890c106ffdec3fb2b0b140b74664765ec8b1
itskundan-01 <2300030371@kluniversity.in>
2026-01-23 15:51:24 +0530

    Fix variable declaration order in unionOfTwoSortedArrays.cpp for clarity

--- CPP/Strivers/Array Problems/Easy/unionOfTwoSortedArrays.cpp
+++ CPP/Strivers/Array Problems/Easy/unionOfTwoSortedArrays.cpp
@@ -19,13 +19,14 @@ int main(){
     int n1,n2;
     cout<<"Enter the size of 1st array: ";
     cin>>n1;
-    int arr1[n1],arr2[n2];
+    int arr1[n1];
     cout<<"Enter the array elements: ";
     for(int i=0;i<n1;i++){
         cin>>arr1[i];
     }
     cout<<"Enter the size of 2nd array: ";
     cin>>n2;
+    int arr2[n2];
     cout<<"Enter the array elements: ";
     for(int i=0;i<n2;i++){
         cin>>arr2[i];
