0d60e3d91b8109536aacc1b2922841c730cbb0a3
itskundan-01 <2300030371@kluniversity.in>
2026-01-25 23:50:31 +0530

    Add comment to clarify string splitting in string.js

--- JavaScript CW/string.js
+++ JavaScript CW/string.js
@@ -2,7 +2,7 @@ let str = "Kundan Kumar";
 console.log(str);
 console.log(str,str.toUpperCase());
 console.log(str,str.toLocaleLowerCase());
-console.log(str=str.split(''));
+console.log(str=str.split('')); //To split the string
 
 //To print the char at 4th index
 console.log(str[4]);
