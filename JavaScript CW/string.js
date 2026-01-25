1b6640800128d63c9717f90b3d9d858a9f8a7999
itskundan-01 <2300030371@kluniversity.in>
2026-01-25 23:51:22 +0530

    Add comment to clarify the purpose of converting string to uppercase in string.js

--- JavaScript CW/string.js
+++ JavaScript CW/string.js
@@ -1,6 +1,6 @@
 let str = "Kundan Kumar";
 console.log(str);
-console.log(str,str.toUpperCase());
+console.log(str,str.toUpperCase());//To change the string to Upper case
 console.log(str,str.toLocaleLowerCase());//To change the string to Lowe case
 console.log(str=str.split('')); //To split the string
 
