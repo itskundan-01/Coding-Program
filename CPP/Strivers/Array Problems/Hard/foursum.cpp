8bfdc585b4fbc906456d99377153b29746977d69
itskundan-01 <2300030371@kluniversity.in>
2026-04-01 22:49:41 +0530

    Add implementation for fourSum problem to find unique quadruplets in an array

--- CPP/Strivers/Array Problems/Hard/foursum.cpp
+++ CPP/Strivers/Array Problems/Hard/foursum.cpp
@@ -8,7 +8,37 @@ int main(){
     cout<<"Enter the size of array: ";
     cin>>n;
     vector<int>nums(n);
+    cout<<"Enter the array elements: ";
     for(int i=0;i<n;i++) cin>>nums[i];
-    
+    cout<<"Enter the Target value: ";
+    int target;
+    cin>>target;
+    vector<vector<int>>ans;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<n;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        for(int j=i+1;j<n;j++){
+            if(j>0 && nums[j]==nums[j-1]) continue;
+            int k=j+1;
+            int l=n-1;
+            while(k<l){
+                long long sum=nums[i]+nums[j]+nums[k]+nums[l];
+                if(sum==target){
+                    ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                    k++;l--;
+                    while(k<l && nums[k]==nums[k-1]) k++;
+                    while(k<l && nums[l]==nums[l+1]) l--;
+                }
+                else if(sum<target)k++;
+                else l--;
+            }
+        }
+    }
+    for(auto triplet : ans){
+        for(int num : triplet){
+            cout << num << " ";
+        }
+        cout << "\n";
+    }
 
 }
